bf931b18826892be910a1944ac5a4dad96c77578
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2025-02-26 21:46:29 +0200

    Added colours.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,19 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        // 'Purple-100': oklch(0.72 0.11 178),
+        // 'Purple-500': 'oklch(55% 0.203 290.4)',
+        // 'Yellow-100': 'oklch(95.34% 0.0204 69.43)',
+        // 'Yellow-500': 'oklch(86.98% 0.1281 81.2)',
+        // 'White': 'oklch(100% 0 0)',
+        // 'Black': 'oklch(18.15% 0 0)',
+        
+        'Purple-100': 'hsl(254, 88%, 90%)',
+        'Purple-500': 'hsl(256, 67%, 59%)',
+        'Yellow-100': 'hsl(31, 66%, 93%)',
+        'Yellow-500': 'hsl(39, 100%, 71%)',
+        'White': 'hsl(0, 0%, 100%)',
+        'Black': 'hsl(0, 0%, 7%)'
       },
     },
   },
