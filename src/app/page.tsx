cff5d213be8e04e464e245e2530e6235393b9eb4
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2025-03-01 21:20:40 +0200

    Finished two card. Added images.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 import Image from "next/image";
 import Card from "./components/Card";
+import Chat_Img from "../../public/assets/images/illustration-ai-content.webp"
+import People_Img from "../../public/assets/images/illustration-audience-growth.webp"
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-100">
       <main className="mx-auto container px-4">
         <div className=" grid grid-flow-row-dense grid-cols-3 grid-rows-3 gap-4">
-            <Card className=" bg-Yellow-100 row-start-1 row-span-5">Create and schedule content quicker.</Card>
-            <Card className="bg-Yellow-500 row-start-6 row-span-5">Write your content using AI.</Card> 
+            <Card className=" px-5 bg-Yellow-100 row-start-1 row-span-5 col-span-1">Create and schedule content quicker.</Card>
+            <Card className="p-5 bg-Yellow-500 row-start-6 row-span-5 col-span-1 flex flex-col gap-10">
+                <p className="text-3xl leading-7 font-medium">Write your content using AI.</p>
+                <Image src={Chat_Img} alt="AI icon" width={200} height={200} className="" />
+            </Card> 
             <Card className="bg-Purple-500 row-start-1 row-span-4 col-span-2 text-White">
                 <span>Social Media 10x Faster with AI</span>
                 <span>Over 4,000 5-star reviews</span>
@@ -18,7 +23,13 @@ export default function Home() {
                 <span>Schedule to social media.</span>
                 <span>Optimize post timings to publish content at the perfect time for your audience.</span>
             </Card>
-            <Card className="bg-White col-span-1 row-span-3">56% faster audience growth</Card>
+            <Card className="px-5 py-7 bg-White col-span-1 row-span-3 flex flex-col justify-between">
+                <div className="flex flex-col gap-5">                
+                    <p className="text-5xl leading-7 font-medium">&gt;56%</p>
+                    <p>faster audience growth</p>
+                </div>
+                <Image src={People_Img} alt="AI icon" width={200} height={200} className="" />
+            </Card>
             <Card className="bg-Purple-500 col-span-2 row-span-3 text-White">Grow followers with non-stop content.</Card>
         </div>
         {/* <div className="grid grid-flow-col grid-rows-10 gap-4">
